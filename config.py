66c3dde4028b275e84b5ea79b4fe0b8a83011bfe
Bartosz Szafranski <b.szafranski@tidio.net>
2024-02-07 22:59:21 +0000

    dataloader merge

---
+++ config.py
@@ -0,0 +1,11 @@
+config = {
+    ## DATA
+    "dataset_name": "rotten_tomatoes",
+    "batch_size": 32,
+    "dataset_local_path": "data/dataset_tokenized",
+    ## tokenizer & model
+    "model_checkpoint": "bert-base-uncased",
+    
+    ## other
+    "seed": 42
+}
