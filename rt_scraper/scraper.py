c8e9bb903a8ff05c3ea28e1c1ad203cd730a2403
Bartosz Szafranski <b.szafranski@tidio.net>
2024-02-28 09:07:57 +0000

    dynamic-loading-proof scraper

--- rt_scraper/scraper.py
+++ rt_scraper/scraper.py
@@ -22,9 +22,9 @@ def parse_args() -> argparse.Namespace:
         description="Scrapes review quotes and review sentiment from Rotten Tomatoes"
     )
     parser.add_argument(
-        "--n_pages_to_scrape",
+        "--n_movies_to_get",
         type=int,
-        help="How many 'popular movies' subpages to iterate over.",
+        help="How many movies to iterate over during retrieval of quotes and scores.",
     )
     args = parser.parse_args()
     return args
@@ -38,16 +38,14 @@ class RottenTomatoesScraper:
     results_folder = "rt_scraper/results/"
 
     def scrape_quotes_and_scores_in_bulk_from_popular_movies_pages(
-        self, n_pages_to_scrape: int
+        self, n_movies_to_get: int
     ):
         with Session() as session:
             self._make_result_folder_if_necessary()
             results_filepath = (
                 self.results_folder + str(dt.datetime.now()).split(".")[0] + ".json"
             )
-            if movie_urls := self.get_list_of_movie_urls_from_multiple_pages(
-                session, n_pages_to_scrape
-            ):
+            if movie_urls := self.get_list_of_movie_urls(session, n_movies_to_get):
                 scraped_data = []
                 try:
                     for movie_url in movie_urls:
@@ -75,37 +73,57 @@ class RottenTomatoesScraper:
             else:
                 logger.critical("Scraper failed during obtaining of movie URLs.")
 
-    def get_list_of_movie_urls_from_multiple_pages(
-        self, session: Session, n_pages_to_scan: int
+    def get_list_of_movie_urls(
+        self, session: Session, n_movies_to_get: int
     ) -> List[Optional[str]]:
-        all_links = []
-        for n_page in range(1, n_pages_to_scan + 1):
-            page_url = self._make_url_for_nth_page_of_popular_movies(n_page)
-            if movies_from_page := self.get_list_of_movie_urls_from_single_page(
-                session, page_url
-            ):
-                all_links += movies_from_page
-        if len(all_links) == 0:
-            logger.critical(
-                f"Did not find any movie URLs from {n_pages_to_scan} populat movies URLs."
-            )
-        return all_links
-
-    def get_list_of_movie_urls_from_single_page(
-        self, session: Session, url: str
-    ) -> List[str] | None:
-        if response := self.scrape_page(session, url):
-            if soup := self.response_to_soup(response):
-                if movie_boxes := soup.find_all("a", {"data-track": "scores"}):
-                    hrefs = [self.base_url + movie["href"] for movie in movie_boxes]
-                    return hrefs
+        url = self.base_url + "/napi/browse/movies_at_home/sort:popular"
+        all_movie_links = set()
+        after = None
+        response_format_error_count = 0
+        while (len(all_movie_links) < n_movies_to_get) or (
+            response_format_error_count < 5
+        ):
+            params = {"after": after}
+            if response := self.scrape_page(session, url, params):
+                try:
+                    resp_json = response.json()
+                except:
+                    response_format_error_count += 1
+                    logger.warning(
+                        f"Response format error for {url} {params}, trial {response_format_error_count} / 5"
+                    )
+                    continue
                 else:
-                    logger.warning(f"Did not find any movie URLs at {url}")
+                    response_format_error_count = 0
+                movie_links = set(
+                    [
+                        self.base_url + movie["mediaUrl"]
+                        for movie in resp_json["grid"]["list"]
+                    ]
+                )
 
-    def scrape_page(self, session: Session, url: str) -> Response | None:
+                if movie_links.issubset(all_movie_links):
+                    logger.warning(
+                        f"No new movies to be added, returning list of {len(all_movie_links)} movie URLs."
+                    )
+                    break
+
+                all_movie_links.update(movie_links)
+
+                if resp_json["pageInfo"]["hasNextPage"]:
+                    after = resp_json["pageInfo"]["endCursor"]
+                else:
+                    logger.warning(
+                        f"Reached maximum number of pages, terminating with list of {len(all_movie_links)} movie URLs."
+                    )
+                    break
+        logger.info(f"Successfully obtained {len(all_movie_links)} URLs to movies.")
+        return all_movie_links
+
+    def scrape_page(self, session: Session, url: str, params={}) -> Response | None:
         try:
             session.headers.update(random.choice(self.headers))
-            response = session.get(url, timeout=5)
+            response = session.get(url, params=params, timeout=15)
             sleep(random.uniform(a=0.5, b=1.5))
             response.raise_for_status()
         except Exception as exc:
@@ -117,7 +135,7 @@ class RottenTomatoesScraper:
                 )
                 raise RuntimeError("Reached max number of consecutive errors")
         else:
-            logger.info(f"Successfully scraped {url}")
+            logger.info(f"Successfully scraped {url} {params}")
             self.consecutive_errors = 0
             return response
 
@@ -149,9 +167,6 @@ class RottenTomatoesScraper:
         except Exception as exc:
             logger.error(f"Could not convert response to soup, reason: {exc}")
 
-    def _make_url_for_nth_page_of_popular_movies(self, n: int) -> str:
-        return self.base_url + f"/browse/movies_at_home/sort:popular?page={n}"
-
     @classmethod
     def _make_result_folder_if_necessary(self) -> None:
         if not os.path.exists(self.results_folder):
@@ -166,4 +181,4 @@ class RottenTomatoesScraper:
 if __name__ == "__main__":
     args = parse_args()
     r = RottenTomatoesScraper()
-    r.scrape_quotes_and_scores_in_bulk_from_popular_movies_pages(args.n_pages_to_scrape)
+    r.scrape_quotes_and_scores_in_bulk_from_popular_movies_pages(args.n_movies_to_get)
